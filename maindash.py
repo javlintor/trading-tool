2a1cc4e33fb66672299e31b03457dc82ac6d05bc
Javier Linares <jlinarestorres2@gmail.com>
2022-05-14 18:58:43 +0200

    foo

--- maindash.py
+++ maindash.py
@@ -2,7 +2,7 @@ import dash
 
 from trading_tool.auth import VALID_USERNAME_PASSWORD_PAIRS
 
-# app = dash.Dash(__name__)
+app = dash.Dash(__name__)
 # auth = dash_auth.BasicAuth(
 #     app,
 #     VALID_USERNAME_PASSWORD_PAIRS
