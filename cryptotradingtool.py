557bebb69b800ced3c5f1153a3a9b0bdecec946d
Javier Linares <jlinarestorres2@gmail.com>
2022-05-14 09:37:56 +0200

    modified app launcher

--- cryptotradingtool.py
+++ cryptotradingtool.py
@@ -1,7 +1,7 @@
 from views.layout import make_layout
 from maindash import app
 
-if __name__ == "__main__":
-    app.layout = make_layout()
-    app.run_server(debug=True)
-    server = app.server
+
+app.layout = make_layout()
+app.run_server(debug=True)
+server = app.server
