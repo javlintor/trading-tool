f464679cd03a5a424c8c8020796433134daa3eb3
Javier Linares <jlinarestorres2@gmail.com>
2022-05-14 13:07:56 +0200

    changed port from default

--- cryptotradingtool.py
+++ cryptotradingtool.py
@@ -1,7 +1,6 @@
 from views.layout import make_layout
 from maindash import app
 
-
 app.layout = make_layout()
-app.run_server(debug=True)
+app.run_server(debug=True, port=8050)
 server = app.server
