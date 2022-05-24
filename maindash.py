8bfb2da4796b35ffccf95fdf4165a4522d0fb5db
Javier Linares <jlinarestorres2@gmail.com>
2022-05-24 21:47:03 +0200

    config dev branch

--- maindash.py
+++ maindash.py
@@ -5,7 +5,3 @@ from trading_tool.auth import VALID_USERNAME_PASSWORD_PAIRS
 
 app = dash.Dash(__name__)
 app.title = "Trading tool"
-auth = dash_auth.BasicAuth(
-    app,
-    VALID_USERNAME_PASSWORD_PAIRS
-)
