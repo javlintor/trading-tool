a5d3c57424a53ed5ae7f12cac4c5e2a6efdaca16
julgalalv <julgalalv@gmail.com>
2022-12-05 09:12:01 +0100

    dev comments added

--- cryptotradingtool.py
+++ cryptotradingtool.py
@@ -1,5 +1,11 @@
+# Import the `make_layout` function from the `layout` module
 from views.layout import make_layout
+
+# Import the Dash app instance
 from maindash import app
 
+# Set the layout of the Dash app to the output of the `make_layout` function
 app.layout = make_layout()
+
+# Assign the server object to the `server` variable
 server = app.server
